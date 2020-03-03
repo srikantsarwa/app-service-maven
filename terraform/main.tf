d4eaa0e0de250eb81a799dc9eb4a5efc682c8c1b
Srikant Sarwa <ssarwa@MININT-BRDI0U7.redmond.corp.microsoft.com>
2020-03-02 16:09:06 -0800

    adding tf files

---
+++ terraform/main.tf
@@ -0,0 +1,28 @@
+resource "azurerm_resource_group" "main" {
+  name     = "SmartQED-RG"
+  location = "westus"
+}
+
+resource "azurerm_app_service_plan" "main" {
+  name                = "ASP-SmartQED"
+  location            = "${azurerm_resource_group.main.location}"
+  resource_group_name = "${azurerm_resource_group.main.name}"
+  kind                = "Linux"
+  reserved            = true
+
+  sku {
+    tier = "Basic"
+    size = "B1"
+  }
+}
+
+resource "azurerm_app_service" "main" {
+  name                = "wardeploysr"
+  location            = "${azurerm_resource_group.main.location}"
+  resource_group_name = "${azurerm_resource_group.main.name}"
+  app_service_plan_id = "${azurerm_app_service_plan.main.id}"
+
+  site_config {
+    linux_fx_version = "TOMCAT|9.0-jre8"
+  }
+}
